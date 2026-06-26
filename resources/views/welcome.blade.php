9970a51e397ae75e660225dc18ac1305aaea24d2
Raendhana Ilmi Vadhiasih <raendhanaiv@student.telkomuniversity.ac.id>
2026-06-26 13:38:26 +0700

    update 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Portfolioo Raendhana Ilmi Vadhiasih</title>
+    <title>Portfolio Raendhana Ilmi Vadhiasih</title>
 
     <script src="https://cdn.tailwindcss.com"></script>
 
